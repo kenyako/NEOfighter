ee7e1c2e7b0c432c0816a08b194c4ca007643efe
MichurinDev <420520052006MfDeveloper>
2022-12-13 20:45:35 +0700

    Добавлен менеджер сцен НЕ РАБОТАЕТ ОТСЛЕЖИВАНИЕ ЕДИНОРАЗОВОГО НАЖАТИЯ НА КНОПКУ

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 
+'''
+Objects
+'''
+
 
 class Button:
     def __init__(self, width, height, inactive_color, active_color):
@@ -15,7 +19,6 @@ class Button:
         # Если курсор в пределах кнопки..
         if btn_x < mouse[0] < btn_x + self.width and \
                 btn_y < mouse[1] < btn_y + self.height:
-
             # Отрисовываем кнопку с цветом <active_color>
             pygame.draw.rect(
                 screen,
@@ -25,7 +28,8 @@ class Button:
 
             # Если по ней кликнули ЛКМ и ключ action задан
             if click[0] == 1 and action is not None:
-                action()  # Запускаем функцию, переданную по ключу action
+                # Запускаем функцию, переданную по ключу action
+                action()
 
         # А ели за пределами кнопки..
         else:
@@ -39,7 +43,7 @@ class Button:
         _fontSize = 30
         # Отрисовываем текст на кнопке
         text_on_btn = pygame.font.Font(
-                                    "./Fonts/retro-land-mayhem.ttf",
+                                    FONT,
                                     _fontSize
                                     )\
             .render(btn_text, True, COLORS['second_text'])
@@ -50,18 +54,22 @@ class Button:
                 )
 
 
-def end_screen(screen, text_on_screen):
+def start_scr_loader():
+    global ACTIVE_SCREEN
+    ACTIVE_SCREEN = 'start'
+
+
+def start_screen(screen, text_on_screen, text_on_button):
     """
     Конечный экран игры, на котором отображается надпись <text_on_screen>
     и кнопка перезапуска
     """
-
     # Закрашиваем окно
     screen.fill(COLORS['main'])
 
     # Отрисовываем текст с результатом
     font_size = 60
-    font = pygame.font.Font('./Fonts/retro-land-mayhem.ttf', font_size)
+    font = pygame.font.Font(FONT, font_size)
     text = font.render(text_on_screen, True, COLORS['text'])
 
     text_x = width // 2 - text.get_width() // 2
@@ -82,15 +90,144 @@ def end_screen(screen, text_on_screen):
     btn.draw(
         width // 2 - btn_width // 2,
         height // 2 - btn_height // 2 + 75,
-        "Restart", start_screen(screen))
+        text_on_button, lvl_1_loader)
 
 
-def start_screen(screen):
+def lvl_1_loader():
+    global ACTIVE_SCREEN
+    ACTIVE_SCREEN = 'lvl_1'
+
+
+def lvl_1(screen):
     """
-    Стартовый экран игры
+    Уровень 1
     """
-    pass
+    # Закрашиваем окно
+    screen.fill(COLORS['main'])
 
+    # Отрисовываем текст с результатом
+    font_size = 60
+    font = pygame.font.Font(FONT, font_size)
+    text = font.render("Level 1", True, COLORS['text'])
+
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2 - 50
+
+    screen.blit(text, (text_x, text_y))
+
+    btn_width, btn_height = 300, 100
+
+    # Создаем экземпляр класса Button (кнопку на экране завершения игры)
+    btn = Button(
+        btn_width, btn_height,
+        COLORS['btn_inactive_color'],
+        COLORS['btn_active_color']
+        )
+
+    # Отрисовываем кнопку на нужных координатах
+    btn.draw(
+        width // 2 - btn_width // 2,
+        height // 2 - btn_height // 2 + 75,
+        "Go to Lvl 2 ->", lvl_2_loader)
+
+
+def lvl_2_loader():
+    global ACTIVE_SCREEN
+    ACTIVE_SCREEN = 'lvl_2'
+
+
+def lvl_2(screen):
+    """
+    Уровень 2
+    """
+    # Закрашиваем окно
+    screen.fill(COLORS['main'])
+
+    # Отрисовываем текст с результатом
+    font_size = 60
+    font = pygame.font.Font(FONT, font_size)
+    text = font.render("Level 2", True, COLORS['text'])
+
+    text_x = width // 2 - text.get_width() // 2 - 50
+    text_y = height // 2 - text.get_height() // 2
+
+    screen.blit(text, (text_x, text_y))
+
+    btn_width, btn_height = 300, 100
+
+    # Создаем экземпляр класса Button (кнопку на экране завершения игры)
+    btn = Button(
+        btn_width, btn_height,
+        COLORS['btn_inactive_color'],
+        COLORS['btn_active_color']
+        )
+
+    # Отрисовываем кнопку на нужных координатах
+    btn.draw(
+        (width - btn_width - 5) // 2 - btn_width // 2,
+        height // 2 - btn_height // 2 + 75,
+        "Go Win ->", win_scr_loader)
+
+    btn2 = Button(
+        btn_width, btn_height,
+        COLORS['btn_inactive_color'],
+        COLORS['btn_active_color']
+        )
+
+    # Отрисовываем кнопку на нужных координатах
+    btn2.draw(
+        (width + btn_width + 5) // 2 - btn_width // 2,
+        height // 2 - btn_height // 2 + 75,
+        "Go Lose ->", lose_scr_loader)
+
+
+def win_scr_loader():
+    global ACTIVE_SCREEN
+    ACTIVE_SCREEN = 'win'
+
+
+def lose_scr_loader():
+    global ACTIVE_SCREEN
+    ACTIVE_SCREEN = 'lose'
+
+
+def end_screen(screen, text_on_screen, text_on_button):
+    """
+    Конечный экран игры, на котором отображается надпись <text_on_screen>
+    и кнопка перезапуска
+    """
+    # Закрашиваем окно
+    screen.fill(COLORS['main'])
+
+    # Отрисовываем текст с результатом
+    font_size = 60
+    font = pygame.font.Font(FONT, font_size)
+    text = font.render(text_on_screen, True, COLORS['text'])
+
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2 - 75
+
+    screen.blit(text, (text_x, text_y))
+
+    btn_width, btn_height = 300, 100
+
+    # Создаем экземпляр класса Button (кнопку на экране завершения игры)
+    btn = Button(
+        btn_width, btn_height,
+        COLORS['btn_inactive_color'],
+        COLORS['btn_active_color']
+        )
+
+    # Отрисовываем кнопку на нужных координатах
+    btn.draw(
+        width // 2 - btn_width // 2,
+        height // 2 - btn_height // 2 + 75,
+        text_on_button, start_scr_loader)
+
+
+'''
+Variables
+'''
 
 # Словарь с цветами, используемыми в коде
 COLORS = {
@@ -102,13 +239,21 @@ COLORS = {
     'btn_active_color': "#4f5257"
 }
 
+FONT = './Fonts/retro-land-mayhem.ttf'
+ACTIVE_SCREEN = 'start'
+
+
+"""
+Main loop
+"""
+
 # Запускаем сие чудо
 if __name__ == '__main__':
     pygame.init()
 
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("NEOfight")
+    pygame.display.set_caption("NEOfighter")
 
     running = True
     while running:
@@ -117,7 +262,16 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
 
-        end_screen(screen, "You Win!")
+        if ACTIVE_SCREEN == "start":
+            start_screen(screen, "NEOfighter", "Start Game!")
+        elif ACTIVE_SCREEN == 'lvl_1':
+            lvl_1(screen)
+        elif ACTIVE_SCREEN == 'lvl_2':
+            lvl_2(screen)
+        elif ACTIVE_SCREEN == 'lose':
+            end_screen(screen, "You Lose!", "Restart")
+        elif ACTIVE_SCREEN == 'win':
+            end_screen(screen, "You Win!", "Restart")
         pygame.display.flip()
 
     pygame.quit()
